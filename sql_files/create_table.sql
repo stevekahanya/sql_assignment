9a2c9a20e386232909bbd7da429a5a48c0cbf329
stevekahanya <wakahusteve@gmail.com>
2026-04-28 19:06:32 +0300

    create table

---
+++ sql_files/create_table.sql
@@ -0,0 +1,9 @@
+-- Create the students table
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    full_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    age INTEGER NOT NULL,
+    course TEXT NOT NULL,
+    created_at TEXT NOT NULL
+);
